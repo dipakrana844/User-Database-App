9047e6542648712dfc6166d138f4d16a58c0343d
dipak.rana <dipakrana4608@gmail.com>
2022-06-13 11:00:26 +0530

    Insert and Delete Completed

--- app/src/main/java/com/executor/userdatabaseapp/DB/UserDAO.kt
+++ app/src/main/java/com/executor/userdatabaseapp/DB/UserDAO.kt
@@ -6,14 +6,14 @@ import androidx.room.*
 @Dao
 interface UserDAO {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-     fun insertUser(user: UserEntity)
+     suspend fun insertUser(user: UserEntity)
 
     @Update
-     fun updateUser(user: UserEntity)
+    suspend fun updateUser(user: UserEntity)
 
     @Delete
-     fun deleteUser(user: UserEntity)
+    suspend fun deleteUser(user: UserEntity)
 
     @Query("select * from user_table")
-    fun getAllUser(): LiveData<List<UserEntity>>
+    suspend fun getAllUser(): LiveData<List<UserEntity>>
 }
--- app/src/main/java/com/executor/userdatabaseapp/DB/UserDatabase.kt
+++ app/src/main/java/com/executor/userdatabaseapp/DB/UserDatabase.kt
@@ -5,6 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import kotlinx.coroutines.internal.synchronized
 
 @Database(entities = [UserEntity::class], version = 1, exportSchema = false)
 @TypeConverters(Convertors::class)
